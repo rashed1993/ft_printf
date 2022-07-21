df666c184607a7ec0ced2631a6b793131250a1fc
rashed1993 <90309061+rashed1993@users.noreply.github.com>
2022-07-21 15:08:19 +0400

    Add files via upload

---
+++ ft_printf.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ral-dhah < ral-dhah@student.42abudhabi.ae> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 16:17:23 by ral-dhah          #+#    #+#             */
+/*   Updated: 2022/07/07 18:44:21 by ral-dhah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int		ft_putchar(char c);
+int		ft_putstr(char *str);
+void	ft_putnbr(int num, int *counter);
+void	ft_puthex(unsigned int num, const char format, int *counter);
+int		ft_hex(unsigned int num, const char format);
+int		ft_len(unsigned int num);
+void	ft_putuint(unsigned int num, int *counter);
+void	ft_putptr(unsigned long long num, int *counter);
+int		ft_ptr(unsigned long long int ptr);
+int		ft_parse(va_list ptr, char c);
+int		ft_printf(const char *str, ...);
+int		ft_print_number(int num);
+int		ft_print_unumber(unsigned int num);
+
+#endif
